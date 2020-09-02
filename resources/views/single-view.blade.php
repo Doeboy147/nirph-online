b1c6668695b3f7cd213668e5c32fd77e4de78519
Lungster <imadecode@gmail.com>
2020-09-02 14:01:00 +0200

    single view

--- resources/views/single-view.blade.php
+++ resources/views/single-view.blade.php
@@ -2,25 +2,42 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 mb-5">
             <div class="card">
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-2 col-md-2">
+                    <a class="btn btn-primary" href="{{ route('home') }}"> Go Back</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mt-5">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-md-8">
                             <div class="card border">
-                                <h5 class="text-center"> Name Surname</h5>
+                                <h5 class="text-center"> {{ $user->name }}</h5>
                                 <div class="card-body text-center">
-                                    <i class="fa fa-user fa-3x"></i>
-                                </div>
-                                <div class="card-footer">
-                                    <div class="row">
-                                        <div class="col-6 col-md-6 col-sm-6">
-                                            <a class="btn btn-primary btn-block" href="/edit">Edit</a>
-                                        </div>
-                                        <div class="col-6 col-md-6 col-sm-6">
-                                            <a class="btn btn-danger btn-block delete" href="/delete">Delete</a>
-                                        </div>
-                                    </div>
+                                    <i class="fa fa-feed fa-3x"></i>
+                                    <h4 class="text-left"> Details</h4>
+
+                                    <table class="table table-striped">
+                                        <tr class="text-left">
+                                            <th>Name</th>
+                                            <td> {{ $user->name }}</td>
+                                        </tr>
+                                        <tr class="text-left">
+                                            <th>Email</th>
+                                            <td> {{ $user->email }}</td>
+                                        </tr>
+                                        <tr class="text-left">
+                                            <th>Date of Birth</th>
+                                            <td> {{ $user->birthday }}</td>
+                                        </tr>
+                                        <tr class="text-left">
+                                            <th>Subscription</th>
+                                            <td> {{ $user->package }}</td>
+                                        </tr>
+                                    </table>
                                 </div>
                             </div>
                         </div>
